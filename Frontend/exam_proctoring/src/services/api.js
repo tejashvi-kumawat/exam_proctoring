7536123dd44a4423c75f6cf937daf594051ffe73
Tejashvi Kumawat <148739094+tejashvi-kumawat@users.noreply.github.com>
2025-06-01 03:24:36 +0530

    Update api.js

--- Frontend/exam_proctoring/src/services/api.js
+++ Frontend/exam_proctoring/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://exam-proctoring-ilwz.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },

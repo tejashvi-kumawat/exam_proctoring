80ec7acd127071a5e615314ba99257b14b12cf54
Tejashvi Kumawat <148739094+tejashvi-kumawat@users.noreply.github.com>
2025-06-01 03:27:39 +0530

    Update urls.py

--- Backend/exam_proctoring/exam_proctoring/urls.py
+++ Backend/exam_proctoring/exam_proctoring/urls.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.http import HttpResponse
+def my_html_view(request):
+    html_content = "<h1>You are on wrong place brother</h1>"
+    return HttpResponse(html_content)
+    
 urlpatterns = [
+    path('/', my_html_view, name='html_view'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/exam/', include('exam_app.urls')),
